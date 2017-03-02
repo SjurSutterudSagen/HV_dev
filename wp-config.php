7ce69b20150721a7de59faa62b6f7ac797739485
sjur <sssagen@hotmail.com>
2017-03-02 10:36:34 +0100

    [VP] Activated VersionPress 4.0-alpha1
    VP-Action: versionpress/activate/4.0-alpha1

--- wp-config.php
+++ wp-config.php
@@ -83,6 +83,7 @@ $table_prefix  = 'wp_';
  */
 define('WP_DEBUG', false);
 
+define('VP_ENVIRONMENT', 'default');
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
